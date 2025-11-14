410338463732402e1710744de1d64526263cb55c
reiroramon <rivansyahorlando0@gmail.com>
2025-11-15 04:11:24 +0700

    bershl

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 
+import { useEffect } from "react";
+import { sdk } from "@farcaster/miniapp-sdk";
+
 export default function Home() {
+  useEffect(() => {
+    // beri tahu Farcaster bahwa UI kamu sudah siap tampil
+    sdk.actions.ready();
+  }, []);
+
   return (
     <main style={{ padding: 20 }}>
       <h1>PUBS BURN Mini App</h1>
