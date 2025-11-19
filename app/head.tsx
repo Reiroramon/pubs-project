3124cd657ef1a17eb47bffcf15576396465c0936
reiroramon <rivansyahorlando0@gmail.com>
2025-11-19 23:03:03 +0700

    heaad

---
+++ app/head.tsx
@@ -0,0 +1,21 @@
+export default function Head() {
+  return (
+    <>
+      <title>PUBS BURN</title>
+      <meta name="description" content="An easy solution to clean your wallet from scam & useless tokens." />
+
+      {/* Open Graph */}
+      <meta property="og:title" content="PUBS BURN" />
+      <meta property="og:description" content="An easy solution to clean your wallet from scam & useless tokens." />
+      <meta property="og:image" content="https://pubs-burn.vercel.app/opengraph-image.png" />
+      <meta property="og:url" content="https://pubs-burn.vercel.app" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="PUBS BURN" />
+      <meta name="twitter:description" content="An easy solution to clean your wallet from scam & useless tokens." />
+      <meta name="twitter:image" content="https://pubs-burn.vercel.app/opengraph-image.png" />
+    </>
+  );
+}
